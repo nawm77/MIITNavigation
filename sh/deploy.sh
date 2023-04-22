14f3a6e3d6ae79fcbac39512bfc64097a8e52925
nawm77 <alpha050603@gmail.com>
2023-04-22 19:11:51 +0300

    correct deploy.sh script for manual docker containers start

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,20 +1,25 @@
 #docker build -t mitnavigation:latest .
 #
 #mkdir -p ./data/postgres
-
+docker rm -f postgresMit
+docker rm -f localtest
+docker network rm my-network
+docker network create my-network
 # Запускаем контейнер Postgres
-#docker run --rm -d \
-#  --name postgresMit \
-#  -p 5432:5432 \
-#  -v /var/lib/postgresql/data \
-#  -e POSTGRES_USER=postgres \
-#  -e POSTGRES_PASSWORD=root \
-#  -e POSTGRES_DB=miit \
-#  postgres:latest
+docker run  -d \
+  --name postgresMit \
+  --network my-network \
+  -p 5432:5432 \
+  -v /var/lib/postgresql/data \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=root \
+  -e POSTGRES_DB=miit \
+  postgres:latest
 
 docker run --privileged -d -p 8080:8080 \
   --name localtest \
-  -e SPRING_DATASOURCE_URL=jdbc:postgresql://host.docker.internal:5432/miit \
+  --network my-network \
+  -e SPRING_DATASOURCE_URL=jdbc:postgresql://postgresMit:5432/miit \
   -e SPRING_DATASOURCE_USERNAME=postgres \
   -e SPRING_DATASOURCE_PASSWORD=root \
   -e SPRING_JPA_HIBERNATE_DDL_AUTO=create \
